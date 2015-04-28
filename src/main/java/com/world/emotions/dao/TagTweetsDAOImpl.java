c07d200da850a922e8ea1321efcda47d3dad85a6
Amit Dubey <amit.dubey2219@gmail.com>
2015-04-28 15:10:06 -0700

    applied limit to gettweets

--- src/main/java/com/world/emotions/dao/TagTweetsDAOImpl.java
+++ src/main/java/com/world/emotions/dao/TagTweetsDAOImpl.java
@@ -1,13 +1,11 @@
 package com.world.emotions.dao;
 
-
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
 import com.world.emotions.resources.TagTweets;
 import com.world.emotions.resources.Tweets;
 import com.world.emotions.util.MySQLConnection;
@@ -17,46 +15,34 @@ public class TagTweetsDAOImpl implements TagTweetsDAO {
 	@Override
 	public TagTweets getTweetsforTag(String tag) {
 		Connection conn = null;
-		TagTweets t= new TagTweets();
-		int d=-1;
-		ArrayList<Tweets> twt= new ArrayList<Tweets>();
+		TagTweets t = new TagTweets();
+		int d = -1;
+		ArrayList<Tweets> twt = new ArrayList<Tweets>();
 
 		try {
-			conn=MySQLConnection.getConnection();
+			conn = MySQLConnection.getConnection();
 
-			String query="select * from trends where lower(hashtag) LIKE lower(?)";
+			String query = "select * from tweets where "
+					+ "hashtag_id=(select id from trends where lower(hashtag) LIKE lower(?)) order by timestamp limit 50";
 
+			PreparedStatement command = conn.prepareStatement(query);
+			command.setString(1, "%" + tag + "%");
+			ResultSet rs = command.executeQuery();
 
-			PreparedStatement command = (PreparedStatement) conn.prepareStatement(query);
-		    command.setString(1, "%"+tag+"%");
-			ResultSet rs= command.executeQuery();
-			
-			
-			while(rs.next()){
-				 d=rs.getInt("id");	
-			}
-			
-			// need to extract tweets based on above id.
-			
-			command=(PreparedStatement) conn.prepareStatement("select * from tweets where hashtag_id=?");
-			command.setInt(1, d);
-			
-			rs=command.executeQuery();
-			
-			while(rs.next()){
-				
-				Tweets obj= new Tweets();
-				System.out.println(rs.getString("text"));
+			while (rs.next()) {
+
+				Tweets obj = new Tweets();
+				// System.out.println(rs.getString("text"));
 				obj.setTweet_Text(rs.getString("text"));
 				obj.setTweet_Id(rs.getLong("tweet_id"));
 				obj.setUrl(rs.getString("profile_img_url"));
 				obj.setUser(rs.getString("user_name"));
-				
+
 				twt.add(obj);
 			}
-			
-		   t.setTweet(twt);
-			
+
+			t.setTweet(twt);
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -65,7 +51,5 @@ public class TagTweetsDAOImpl implements TagTweetsDAO {
 		return t;
 
 	}
-
-
 
 }
